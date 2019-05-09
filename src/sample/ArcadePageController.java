f3f354a54200db7a1e5b3b1aeb919dd265619ecc
Gokce <gokcessefa@gmail.com>
2019-05-09 17:34:04 +0300

    Update ArcadePageController.java

--- src/sample/ArcadePageController.java
+++ src/sample/ArcadePageController.java
@@ -34,73 +34,93 @@ public class ArcadePageController implements Initializable {
         Stage stg2 = (Stage) backButton.getScene().getWindow();
         stg2.close();
     }
-    public void sendArcadeButton(){
+
+    public void sendArcadeButton() {
         for (int i = 1; i <= 40; i++) {
             final int level = i;
             Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(level)));
         }
     }
-    public void easy1(){
+
+    public void easy1() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(1)));
     }
-    public void easy2(){
+
+    public void easy2() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(2)));
     }
-    public void easy3(){
+
+    public void easy3() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(3)));
     }
-    public void easy4(){
+
+    public void easy4() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(4)));
     }
-    public void easy5(){
+
+    public void easy5() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(5)));
     }
-    public void med1(){
+
+    public void med1() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(6)));
     }
-    public void med2(){
+
+    public void med2() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(7)));
     }
-    public void med3(){
+
+    public void med3() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(8)));
     }
-    public void med4(){
+
+    public void med4() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(9)));
     }
-    public void med5(){
+
+    public void med5() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(10)));
     }
-    public void hard1(){
+
+    public void hard1() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(11)));
     }
-    public void hard2(){
+
+    public void hard2() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(12)));
     }
-    public void hard3(){
+
+    public void hard3() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(13)));
     }
-    public void hard4(){
+
+    public void hard4() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(14)));
     }
-    public void hard5(){
+
+    public void hard5() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(15)));
     }
-    public void h1(){
+
+    public void h1() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(16)));
     }
-    public void h2(){
+
+    public void h2() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(17)));
     }
-    public void h3(){
+
+    public void h3() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(18)));
     }
-    public void h4(){
+
+    public void h4() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(19)));
     }
-    public void h5(){
+
+    public void h5() {
         Main.mainStage.setScene(new ArcadeGameScene(new GameLevel(20)));
     }
-
 
 
 }
