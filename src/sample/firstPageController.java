e07461963578d5a23a1c99f1872e939fc0451f04
Mustafa Azyoksul <32205084+user1444@users.noreply.github.com>
2019-05-10 06:52:22 +0300

    Revert "as"
    This reverts commit 54df77fdb5460721df5c71d31b42925f33c22192.

--- src/sample/firstPageController.java
+++ src/sample/firstPageController.java
@@ -7,16 +7,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-
-import java.awt.*;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
